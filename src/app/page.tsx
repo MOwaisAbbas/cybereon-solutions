d26d56310d799cc0f22617d9892c463046191303
mowaisabbas <imowaispatel@gmail.com>
2025-03-23 02:26:00 +0500

    home page complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/views/Hero";
 import Services from "@/views/Services";
+import WhyChoose from "@/views/WhyChoose";
 
 export default function Home() {
   return (
     <>
     <Hero/>
     <Services/>
+    <WhyChoose/>
     </>
   );
 }
